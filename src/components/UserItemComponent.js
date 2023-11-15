a7660fdf5bb9fad76c9559e3c705ba534849999f
adifahmad <khaironi20@gmail.com>
2023-11-15 12:01:00 +0700

    updateC35-redux

--- src/components/UserItemComponent.js
+++ src/components/UserItemComponent.js
@@ -1,6 +1,5 @@
 import './style.css'
-import axios from 'axios'
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useEffect, useState } from 'react'
 import { removeUser } from '../actions/users'
 import { loadUser } from "../actions/users"
@@ -9,17 +8,21 @@ function AvatarImage({ photos }) {
     return <img src={photos == null ? `/usertie.png` : `/${photos}`} alt="logo" className="Avatar" />
 }
 
-function ButtonEdit() {
-    return <i class="fa-solid fa-pen-to-square"></i>
+function ButtonEdit({ edit }) {
+    return <i class="fa-solid fa-pen-to-square" onClick={edit}></i>
 }
 
 function ButtonDelete({ remove }) {
     return <i class="fa-solid fa-trash" onClick={remove}></i>
 }
 
+function ButtonSave({ save }) {
+    return <i class="fa-solid fa-floppy-disk" onClick={save}></i>
+}
 
 export default function UserItemComponent() {
 
+    const [isEdit, setIsEdit] = useState(false)
     const item = useSelector(state => state.users)
     const dispatch = useDispatch()
 
@@ -30,33 +33,66 @@ export default function UserItemComponent() {
 
     const result = []
 
-    return (
-        <div className="itemCardContainer">
-            {item.map((user) => {
-                return (
-                    <>
-                        <div className="itemContainer">
-                            <div className='avatarContainer'>
-                                <AvatarImage photos={user.avatar} />
-                            </div>
-                            <div className='formContainer'>
-                                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"></link>
-                                <p>{user.name}</p>
-                                <p>{user.phone}</p>
-                                <div className='formButton'>
-                                    <div className='formButtonEdit'>
-                                    <ButtonEdit />
-                                    </div>
-                                    <ButtonDelete remove={() => dispatch(removeUser( user.id ))} />
+    if (isEdit) {
+        return (
+            <div className="itemCardContainer">
+                {item.map((user) => {
+                    return (
+                        <>
+                            <div className="itemContainer">
+                                <div className='avatarContainer'>
+                                    <AvatarImage photos={user.avatar} />
                                 </div>
+                                <div className='formContainer'>
+                                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"></link>
+                                    <div>
+                                        <input type='text' className='formInputName'></input>
+                                    </div>
+                                    <div>
+                                        <input type='text' className='formInputPhone'></input>
+                                    </div>
+                                    <div className='formButton'>
+                                        <div className='formButtonEdit'>
+                                            <ButtonSave save={() => (setIsEdit(false))} />
+                                        </div>
+                                    </div>
+                                </div>
+                                {result}
                             </div>
-                            {result}
-                        </div>
-                    </>
-                );
-            })}
-        </div>
-    )
+                        </>
+                    );
+                })}
+            </div>
+        )
+    } else {
+        return (
+            <div className="itemCardContainer">
+                {item.map((user) => {
+                    return (
+                        <>
+                            <div className="itemContainer">
+                                <div className='avatarContainer'>
+                                    <AvatarImage photos={user.avatar} />
+                                </div>
+                                <div className='formContainer'>
+                                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"></link>
+                                    <p>{user.name}</p>
+                                    <p>{user.phone}</p>
+                                    <div className='formButton'>
+                                        <div className='formButtonEdit'>
+                                            <ButtonEdit edit={() => (setIsEdit(true))} />
+                                        </div>
+                                        <ButtonDelete remove={() => dispatch(removeUser(user.id))} />
+                                    </div>
+                                </div>
+                                {result}
+                            </div>
+                        </>
+                    );
+                })}
+            </div>
+        )
+    }
 
 
 }
